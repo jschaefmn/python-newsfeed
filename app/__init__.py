8331a08d4415e5f844ffc5fd898875d777cb9f5b
Jake Schaefbauer <jschaefmn@gmail.com>
2022-12-07 11:21:53 -0600

    added filters to post-info

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,9 @@
 from app.routes import home, dashboard
 from flask import Flask
 from app.db import init_db
+from app.utils import filters
+
+
 
 def create_app(test_config=None):
   # Set up app config
@@ -19,5 +22,8 @@ def create_app(test_config=None):
   app.register_blueprint(dashboard)
 
   init_db(app)
+  app.jinja_env.filters['format_url'] = filters.format_url
+  app.jinja_env.filters['format_date'] = filters.format_date
+  app.jinja_env.filters['format_plural'] = filters.format_plural
 
   return app
